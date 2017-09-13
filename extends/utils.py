005eb0d2c9d5f3b8ef0862d09505613a6f8fed76
bernieyangmh <berniey@163.com>
2017-09-13 10:12:29 +0800

    forbidden import

--- extends/utils.py
+++ extends/utils.py
@@ -13,6 +13,10 @@ from data_analysis.util import print_summary_information
 
 tem_dir = tempfile.mkdtemp(suffix='_python_code', prefix='website_')
 python_file_index = 0
+exec_3 = "/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6"
+exec_2 = "/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python"
+re_date = re.compile(r"^\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2}$")
+re_timestamp = re.compile(r"^[0-9]{1,45}$")
 
 def data_analysis(path, num):
     print "data_analysis"
@@ -23,8 +27,7 @@ def data_analysis(path, num):
 
 
 def get_timestamp_date(timestamp_data):
-    re_date = re.compile(r"^\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2}$")
-    re_timestamp = re.compile(r"^[0-9]{1,45}$")
+
     if re_timestamp.match(timestamp_data):
         timestamp_data_res = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(timestamp_data)))
 
@@ -42,13 +45,13 @@ def url_decode_encode(url):
 def python_script_run(version, code):
     global python_file_index
 
-    exec_3 = "/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6"
-    exec_2 = "/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python"
 
     python_file_name = "python_%d" % python_file_index
     python_file_index += 1
     python_file_path = os.path.join(tem_dir, '%s.py' % python_file_name)
     code = code.encode('utf-8')
+    if code.find("import") != -1:
+        return "抱歉，不能使用import关键字"
 
     with open(python_file_path, 'w') as f:
         f.write(code)
